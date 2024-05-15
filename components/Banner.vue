01c67118ed2b9b9f8ec902da6fcde953716faa76
AndressaSales <andressaccs2001@gmail.com>
2024-05-15 14:42:24 -0300

    flores

---
+++ components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 id="logo" class="text-center text-9xl p-2.5 pb-16">FloresBellas</h1>
+    </div>
+</template>
+
+<style>
+    #logo{
+        background-image: url("https://1.bp.blogspot.com/-v6qv4xMvllw/WKAjmoIkmYI/AAAAAAAAA6s/eoP2UAizsOIRliU5AR4QQR_nF2PSv93CACLcB/s2560/papeldeparede-campo-flores.jpg");
+        background-size: cover;
+        -webkit-background-clip: text;
+        background-clip: text;
+        color: transparent;
+    }
+    @media only screen and (max-width: 700px){
+        #logo{
+            font-size: 65px;
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
-        <Carrosel/>
-        <h1 class="text-fuchsia-500 text-3xl text-center pt-4 font-bold">Para os amantes de <mark class="bg-transparent text-red-600">flores</mark></h1>
-        <h3 class="text-center text-xl font-bold">Surpreenda alguem especial com estes lindos buquês</h3>
+        <h1 id="text1" class="text-fuchsia-500 text-3xl text-center pt-4 font-bold">Para os amantes de <mark class="bg-transparent text-red-600">flores</mark></h1>
+        <h3 id="text2" class="text-center text-xl font-bold">Surpreenda alguem especial com estes lindos buquês</h3>
         <Menu/>
+        <Banner/>
     </div>
 </template>
+<style scoped>
+    @media only screen and (max-width: 700px){
+        #text1{
+            text-align: center;
+            font-size: 1.5rem;
+        }
+        #text2{
+            text-align: center;
+            font-size: 1rem;
+        }
+    }
+</style>
